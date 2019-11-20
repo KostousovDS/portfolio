46baeaa969bdd9feb0566179ca30a4e5c3c8a842
DEMID <kostousovds@gmail.com>
2019-11-20 17:27:33 +0300

    Сделали Header

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict'
+
+var gulp = require('gulp'); // определим переменную gulp
+var sass = require('gulp-sass'); // определим переменную sass
+var rigger = require('gulp-rigger'); // определим переменную rigger
+var watch = require('gulp-watch'); // определим переменную watch
+var copy = require('gulp-copy'); // определим переменную copy
+var connect = require('gulp-connect'); // определим переменную connect
+
+sass.compiler = require('node-sass'); // определим переменную sass.compiler
+
+gulp.task('sass', function () {
+    return gulp.src('./src/styles/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./build/styles/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    watch(['./src/styles/**/*.scss', './src/styles/*.scss'], gulp.series('sass'));
+    watch(['./src/template/**/*.html', './src/index.html'], gulp.series('rigger'));
+    watch('./src/js/**/*.js', gulp.series('js'));
+});
+
+gulp.task('rigger', function () {
+    return gulp.src('./src/index.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('./build/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+    return gulp.src('./src/js/**.js')
+        .pipe(rigger())
+        .pipe(gulp.dest('./build/js/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('copy', function () {
+    gulp.src('./src/images/**/**.*')
+        .pipe(gulp.dest('./build/images/'));
+    gulp.src('./src/fonts/**/**.*')
+        .pipe(gulp.dest('./build/fonts/'));
+    gulp.src('./src/libs/**/**.*')
+        .pipe(gulp.dest('./build/libs/'))
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'build',
+        livereload: true,
+        port: 9002
+    });
+});
+
+gulp.task('default', gulp.parallel('watch', 'connect', 'rigger', 'copy', 'sass', 'js'));
---
+++ src/js/common.js
@@ -0,0 +1,7 @@
+function down() {
+    var a = document.getElementById('dropup');
+    if (a.style.display == 'none')
+        a.style.display = 'block'
+    else if (a.style.display == 'block')
+        a.style.display = 'none';
+};
